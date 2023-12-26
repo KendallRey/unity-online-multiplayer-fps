7d64b6c4d31fc34bff7cc6ec13d7c5b677f33b32
UVMapping <107897863+KendallRey@users.noreply.github.com>
2023-12-26 12:34:01 +0800

    update material and layer manager

--- Assets/Controller/LayerManager.cs
+++ Assets/Controller/LayerManager.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 public class LayerManager : PlayerView
 {
-    const string WEAPONS_LAYER = "Weapons";
+    [SerializeField] string LayerName;
     [SerializeField] GameObject[] objectsToApplyLayer;
-
+    [SerializeField] bool ignoreIfMine = true;
     private void Start()
     {
         if(!view.IsMine) return;
         foreach(GameObject obj in objectsToApplyLayer)
         {
-            obj.layer = LayerMask.NameToLayer(WEAPONS_LAYER);
+            obj.layer = LayerMask.NameToLayer(LayerName);
         }
     }
 
